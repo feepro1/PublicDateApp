dd4b1f6c67a2a28308cb4a425ce643b5d5322376
vadwwxz <vadwwxz@gmail.com>
2023-01-05 17:33:40 +0200

    Created business logic for RegisterViewModel

--- register/src/main/java/com/main/register/presentation/viewmodel/RegisterViewModel.kt
+++ register/src/main/java/com/main/register/presentation/viewmodel/RegisterViewModel.kt
@@ -3,6 +3,8 @@ package com.main.register.presentation.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.main.core.dispatchers.DispatchersList
+import com.main.core.exception.*
+import com.main.core.state.InputTextState
 import com.main.register.data.entities.RegisterData
 import com.main.register.domain.navigation.RegisterNavigation
 import com.main.register.domain.usecase.RegisterUseCase
@@ -17,9 +19,30 @@ class RegisterViewModel(
 ) : ViewModel() {
 
     fun register(registerData: RegisterData) {
-//        viewModelScope.launch {
-//            registerUseCase.execute(registerData)
-//        }
+        viewModelScope.launch(dispatchers.io()) {
+            val result = registerUseCase.execute(registerData)
+            if (result.data == true) {
+                //todo navigate to main fragment
+                return@launch
+            }
+            when (result.exception) {
+                is PasswordException -> {
+                    registerCommunication.managePasswordError(InputTextState.ShowError(result.exception?.message!!))
+                }
+                is EmailException -> {
+                    registerCommunication.manageEmailError(InputTextState.ShowError(result.exception?.message!!))
+                }
+                is ConfirmPasswordException -> {
+                    registerCommunication.manageConfirmPasswordError(InputTextState.ShowError(result.exception?.message!!))
+                }
+                is FirstNameException -> {
+                    registerCommunication.manageFirstNameError(InputTextState.ShowError(result.exception?.message!!))
+                }
+                is LastNameException -> {
+                    registerCommunication.manageLastNameError(InputTextState.ShowError(result.exception?.message!!))
+                }
+            }
+        }
     }
 
 }
