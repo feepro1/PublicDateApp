1fc123436febb0a3b32fcb23c165ab34726e3dfc
sliderzxc <100761978+sliderzxc@users.noreply.github.com>
2023-01-10 21:32:04 +0200

    develop v1.0.1 (#5)
    * Created architecture, and DatingFragment
    
    * Created DeepLink, and way to navigation

--- core/src/main/java/com/main/core/navigation/DeepLinks.kt
+++ core/src/main/java/com/main/core/navigation/DeepLinks.kt
@@ -5,4 +5,5 @@ import androidx.core.net.toUri
 object DeepLinks {
     val REGISTER_DEEP_LINK = "swapLike://register".toUri()
     val LOGIN_DEEP_LINK = "swapLike://login".toUri()
+    val DATING_DEEP_LINK = "swapLike://dating".toUri()
 }
