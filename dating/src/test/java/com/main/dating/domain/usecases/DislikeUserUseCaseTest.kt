26753375c53d3d8b0fef36c006ca540642c57a9e
vadwwxz <vadwwxz@gmail.com>
2023-01-13 14:02:04 +0200

    Created unit tests for DislikeUserUseCase (TDD), and Updated unit tests for LikeUserUseCase (TDD)

--- dating/src/test/java/com/main/dating/domain/usecases/DislikeUserUseCaseTest.kt
+++ dating/src/test/java/com/main/dating/domain/usecases/DislikeUserUseCaseTest.kt
@@ -7,34 +7,36 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.mock
 
-class LikeUserUseCaseTest {
+class DislikeUserUseCaseTest {
 
     private val manageUserRepository = mock<ManageUserRepository>()
-    private val likeUserUseCase = LikeUserUseCase(
+    private val dislikeUserUseCase = DislikeUserUseCase(
         manageUserRepository = manageUserRepository
     )
-
     private val user = User(
         firstName = "Vadym", lastName = "Hrynyk",
         avatarUrl = "https://some", email = "vadwwxz@mail.com"
     )
 
     @Test
-    fun `test successful like user`() = runBlocking {
-        Mockito.`when`(manageUserRepository.likeUser(user)).thenReturn(Resource.Success(true))
-        val result = likeUserUseCase.execute(user)
+    fun `test successful dislike user`() = runBlocking {
+        Mockito.`when`(manageUserRepository.dislikeUser(user)).thenReturn(
+            Resource.Success(true)
+        )
+        val result = dislikeUserUseCase.execute(user)
 
         Assertions.assertTrue(result.data == true)
     }
 
     @Test
     fun `test failure like user, internet is unavailable`() = runBlocking {
-        Mockito.`when`(manageUserRepository.likeUser(user)).thenReturn(
+        Mockito.`when`(manageUserRepository.dislikeUser(user)).thenReturn(
             Resource.Error(false, NetworkException(INTERNET_IS_UNAVAILABLE))
         )
-        val result = likeUserUseCase.execute(user)
+        val result = dislikeUserUseCase.execute(user)
         val finishResult = result.exception.message == INTERNET_IS_UNAVAILABLE
 
         Assertions.assertTrue(finishResult)
     }
+
 }
