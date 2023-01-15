996f704feda5ec3bf9e96383e863a9c4b83ecc30
vadwwxz <vadwwxz@gmail.com>
2023-01-15 18:21:38 +0200

    Created unit tests for SaveUserInfoRepository (TDD)

---
+++ profile/src/test/java/com/example/profile/domain/firebase/SaveUserInfoRepositoryTest.kt
@@ -0,0 +1,35 @@
+package com.example.profile.domain.firebase
+
+import com.main.core.Resource
+import com.main.core.exception.NetworkException
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class SaveUserInfoRepositoryTest {
+
+    private val saveUserInfoRepository = mock<SaveUserInfoRepository>()
+
+    @Test
+    fun `test successful save user info`() = runBlocking {
+        Mockito.`when`(saveUserInfoRepository.saveUserInfo(UserInfo())).thenReturn(
+            Resource.Success(true)
+        )
+        val result = saveUserInfoRepository.saveUserInfo(UserInfo())
+
+        Assertions.assertTrue(result.data == true)
+    }
+
+    @Test
+    fun `test failure save user info, internet is not available`() = runBlocking {
+        Mockito.`when`(saveUserInfoRepository.saveUserInfo(UserInfo())).thenReturn(
+            Resource.Error(false, NetworkException(INTERNET_IS_UNAVAILABLE))
+        )
+        val result = saveUserInfoRepository.saveUserInfo(UserInfo())
+
+        Assertions.assertTrue(result.exception?.message == INTERNET_IS_UNAVAILABLE)
+    }
+
+}
