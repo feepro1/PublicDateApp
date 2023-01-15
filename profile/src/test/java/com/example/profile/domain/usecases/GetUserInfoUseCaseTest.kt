8cd6bf9c9427f8f58f6414b04a4b91110d8f3c9a
vadwwxz <vadwwxz@gmail.com>
2023-01-15 18:05:16 +0200

    Created unit tests for GetUserInfoUseCase (TDD), and initialized application architecture

---
+++ profile/src/test/java/com/example/profile/domain/usecases/GetUserInfoUseCaseTest.kt
@@ -0,0 +1,37 @@
+package com.example.profile.domain.usecases
+
+import com.main.core.Resource
+import com.main.core.exception.NetworkException
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class GetUserInfoUseCaseTest {
+
+    private val getUserInfoRepository = mock<GetUserInfoRepository>()
+    private val getUserInfoUseCase = GetUserInfoUseCase(
+        getUserInfoRepository = getUserInfoRepository
+    )
+
+    @Test
+    fun `test successful get user info`() = runBlocking {
+        Mockito.`when`(getUserInfoRepository.receiveUserInfo()).thenReturn(
+            Resource.Success(UserInfo())
+        )
+        val userInfo = getUserInfoUseCase.execute()
+
+        Assertions.assertTrue(userInfo.data != null)
+    }
+
+    @Test
+    fun `test failure get user info, internet is not available`() = runBlocking {
+        Mockito.`when`(getUserInfoRepository.receiveUserInfo()).thenReturn(
+            Resource.Error(null, NetworkException(INTERNET_IS_UNAVAILABLE))
+        )
+        val userInfo = getUserInfoUseCase.execute()
+
+        Assertions.assertTrue(userInfo.exception?.message == INTERNET_IS_UNAVAILABLE)
+    }
+}
