c0d04724ec1d48c19e7a39d2103183fe5576508d
vadwwxz <vadwwxz@gmail.com>
2023-01-16 13:21:56 +0200

    Created design for fragment_profile

--- profile/src/main/java/com/main/profile/data/entities/UserInfo.kt
+++ profile/src/main/java/com/main/profile/data/entities/UserInfo.kt
@@ -3,4 +3,12 @@ package com.main.profile.data.entities
 data class UserInfo(
     val firstName: String = "",
     val lastName: String = "",
+    val avatarUrl: String = "",
+    val email: String = "",
+    val uid: String = "",
+    val likeFromAnotherUser: HashMap<String, String?> = hashMapOf(),
+    val age: Int? = null,
+    val city: String = "",
+    val region: String = "",
+    val aboutMe: String = ""
 )
