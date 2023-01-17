52c5492ced9ad624c56d4059222004e95a86f100
vadwwxz <vadwwxz@gmail.com>
2023-01-17 16:25:10 +0200

    Created unit tests for GetAllChatsUseCase (TDD), and created new module

---
+++ chats/src/test/java/com/main/chats/domain/usecases/GetAllChatsUseCaseTest.kt
@@ -0,0 +1,44 @@
+package com.main.chats.domain.usecases
+
+import com.main.core.Resource
+import com.main.core.exception.NetworkException
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.jupiter.api.Assertions
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+class GetAllChatsUseCaseTest {
+
+    private val chatsRepository = mock<ChatsRepository>()
+    private val getAllChatsUseCase = GetAllChatsUseCase(
+        chatsRepository = chatsRepository
+    )
+
+    @Test
+    fun `test successful get all chats`() = runBlocking {
+        Mockito.`when`(chatsRepository.getAllChats()).thenReturn(
+            Resource.Success(listOf(Chat()))
+        )
+        val result = getAllChatsUseCase.execute()
+        Assertions.assertTrue(result.data.isNotEmpty())
+    }
+
+    @Test
+    fun `test successful get all chats, but chats is empty`() = runBlocking {
+        Mockito.`when`(chatsRepository.getAllChats()).thenReturn(
+            Resource.Success(emptyList())
+        )
+        val result = getAllChatsUseCase.execute()
+        Assertions.assertTrue(result.data.isNotEmpty())
+    }
+
+    @Test
+    fun `test failure get all chats, internet is not available`() = runBlocking {
+        Mockito.`when`(chatsRepository.getAllChats()).thenReturn(
+            Resource.Error(emptyList(), NetworkException(INTERNET_IS_UNAVAILABLE))
+        )
+        val result = getAllChatsUseCase.execute()
+        Assertions.assertTrue(result.exception?.message == INTERNET_IS_UNAVAILABLE)
+    }
+}
