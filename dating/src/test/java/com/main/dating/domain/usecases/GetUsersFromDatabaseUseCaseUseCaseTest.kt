8f762aa4feca030e86e1e5f9ee610cc54b298cb6
vadwwxz <vadwwxz@gmail.com>
2023-01-21 18:24:48 +0200

    Corrected dating unit tests

--- dating/src/test/java/com/main/dating/domain/usecases/GetUsersFromDatabaseUseCaseUseCaseTest.kt
+++ dating/src/test/java/com/main/dating/domain/usecases/GetUsersFromDatabaseUseCaseUseCaseTest.kt
@@ -4,7 +4,7 @@ import com.main.core.Resource
 import com.main.core.exception.NetworkException
 import com.main.dating.data.entities.User
 import com.main.dating.data.exception.message.DatingExceptionMessages.INTERNET_IS_UNAVAILABLE
-import com.main.dating.data.realization.firebase.database.DatabaseRepository
+import com.main.dating.domain.firebase.database.DatabaseRepository
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.jupiter.api.Assertions
